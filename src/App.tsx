fe0a820df4063c1673e0303afe152491b070aa8c
DenisAfa <denisafanasevm92@mail.ru>
2021-03-10 21:20:16 +0300

    WIP project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import { Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainPage from "./components/MainPage";
+import Countries from "./components/Countries";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className='App'>
+      <Header />
+      <Route exact path='' render={() => <MainPage />} />
+      <Route path='/country:countryName?' render={() => <Countries />} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Countries/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Countries: React.FC = () => {
+  return <div></div>;
+};
+
+export default Countries;
